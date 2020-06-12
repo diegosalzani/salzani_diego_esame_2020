22e2080820ecaa11f59b4ca60556a11e13bbba4d
diegosalzani <58070099+diegosalzani@users.noreply.github.com>
2020-06-12 15:15:44 +0200

    Update verona_post.ddl.sql

--- SQL/verona_post.ddl.sql
+++ SQL/verona_post.ddl.sql
@@ -1,33 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Giu 10, 2020 alle 17:25
--- Versione del server: 10.4.11-MariaDB
--- Versione PHP: 7.4.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `verona_post`
---
+
 CREATE DATABASE verona_post;
 USE verona_post;
--- --------------------------------------------------------
 
---
+
 -- Struttura della tabella `risposte`
---
 
 CREATE TABLE `risposte` (
   `id_utente` int(11) NOT NULL,
@@ -37,19 +15,16 @@ CREATE TABLE `risposte` (
   `risposta3` varchar(220) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
---
+
 -- Struttura della tabella `sondaggio`
---
 
 CREATE TABLE `sondaggio` (
   `id_sondaggio` int(11) NOT NULL,
   `nome_sondaggio` varchar(220) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
+
 -- Struttura della tabella `utenti`
---
 
 CREATE TABLE `utenti` (
   `id` int(11) NOT NULL,
@@ -59,57 +34,46 @@ CREATE TABLE `utenti` (
   `data_iscrizione` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indici per le tabelle scaricate
---
 
---
--- Indici per le tabelle `risposte`
---
+
+-- Indici per la tabella `risposte`
+
 ALTER TABLE `risposte`
   ADD KEY `id_utente` (`id_utente`),
   ADD KEY `id_sondaggio` (`id_sondaggio`);
 
---
--- Indici per le tabelle `sondaggio`
---
+
+-- Indici per la tabella `sondaggio`
+
 ALTER TABLE `sondaggio`
   ADD PRIMARY KEY (`id_sondaggio`);
 
---
--- Indici per le tabelle `utenti`
---
+
+-- Indici per la tabella `utenti`
+
 ALTER TABLE `utenti`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT per le tabelle scaricate
---
 
---
+
+
 -- AUTO_INCREMENT per la tabella `sondaggio`
---
+
 ALTER TABLE `sondaggio`
   MODIFY `id_sondaggio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
+
 -- AUTO_INCREMENT per la tabella `utenti`
---
+
 ALTER TABLE `utenti`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
---
--- Limiti per le tabelle scaricate
---
 
---
+
 -- Limiti per la tabella `risposte`
---
+
 ALTER TABLE `risposte`
   ADD CONSTRAINT `risposte_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `risposte_ibfk_2` FOREIGN KEY (`id_sondaggio`) REFERENCES `sondaggio` (`id_sondaggio`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
